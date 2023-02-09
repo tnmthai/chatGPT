111d929b40fcfde83f003f3fb9ff3f614ba5972c
Dr. Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-02-09 15:45:41 +1300

    Update model.py

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ class GeneralModel:
 
             # arguments to send the API
         kwargs = {
-            "engine": "code-davinci-003",
+            "engine": "text-davinci-003",
             "temperature": 0.9,
             "max_tokens": 1024,            
             "top_p":1.0,            
